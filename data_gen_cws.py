39e39b857b9ddf7e2af1f56a2c06e034aba00a35
asimkazmi <asim.kazmi@elastica.co>
2014-11-21 16:59:37 +0500

    Added support for creating UserIds on the fly. Also, updated some logic for better zip handling

--- data_gen_cws.py
+++ data_gen_cws.py
@@ -13,26 +13,29 @@ Revisions:
 """
 
 import os
-import re
 import sys
+import re
 import time
 import random
 import logging
 import gzip
+import ipaddress
+from time import gmtime, strftime
+import time
 
-logger = logging.basicConfig()
+logger = logging.getLogger("Data Gen Logger:")
 timestamp_format = '2014-11-01 00:00:01'
 
 
 def main(no_users, logs_per_user, invalid_rows, format):
     try:
         data_path = "../cws_data/"
-        filename = str(int(time.time())) + "-day-" + str(timestamp_format[5]) + "-data"
+        filename = str(int(time.time())) + "-day-" + str(timestamp_format[8]) + str(timestamp_format[9]) + "-data"
         new_file = open(data_path + filename, 'wb')
         user_ip_file = open("user_ips.csv", 'r')
         service_url_file = open("unique_urls_cws.csv", "r")
         useragent_file = open("unique_user_agents.csv", "r")
-        destip_file = open("unique_dest_ip_1.csv", "r")
+        destip_file = open("dest_ips_valid.csv", "r")
         no_users = int(no_users)
         logs_per_user = int(logs_per_user)
         invalid_rows = int(invalid_rows)
@@ -60,7 +63,8 @@ def write_cws_file(filename, no_users, logs_per_user, invalid_rows, users, servi
     row_index = 0
     service_range_start = 1
     service_range_end = 100
-
+    intipadd = 168427521
+    prepare_stream_logger(logger, None, logging.DEBUG)
     try:
         print "**************************************"
         print "Generating Valid Data..."
@@ -71,20 +75,21 @@ def write_cws_file(filename, no_users, logs_per_user, invalid_rows, users, servi
             user_index = 0
             agent_index = 0
             dest_index = 0
-
+            userip = ipaddress.IPv4Address(intipadd)
             # Loop to Generate Log per user.
+
             for logs in range(0, logs_per_user):
 
                 # Randomizing sent and received bytes
                 s_bytes = random.randint(1000, 5000)
                 r_bytes = random.randint(1000, 5000)
-                service_list = random.randint(service_range_start, service_range_end)
+                service_list = random.randint(service_range_start, service_range_start)
 
                 # Writing the Data Rows for each user. Parameterized Fields include Username, Service name, Sent Bytes,
                 # Received bytes, User Agent, destination IP and uri scheme. User Agensts are selected one for each user.
                 new_file.write(
                     "%s\t%s\t\t%s\tGET\t%s\t%s\t80\t\t\t%s\t-\t%d\t%d\t200\timage/jpeg\t%s\tc:infr\tdefault\tallow\t\t\t180.143.124.98\t1278716032\t\n" % (
-                        get_new_date(timestamp_format), users[user].strip(), users[user].strip(),
+                        get_new_date(timestamp_format), str(userip), str(userip),
                         services[service_list].split(":")[0],
                         services[service_list].split("//")[1].strip(), user_agent[agent_index].strip(), s_bytes,
                         r_bytes, dest_ip[dest_index].strip()))
@@ -95,15 +100,23 @@ def write_cws_file(filename, no_users, logs_per_user, invalid_rows, users, servi
                 agent_index += 1
 
                 # Since the total number of user agents may not be equal to the total number of users.
-                if agent_index == len(user_agent):
+                if agent_index >= len(user_agent) - 1:
                     agent_index = 0
 
                 # Since the total number of destination IPs may not be equal to the total number of users.
-                if dest_index == len(dest_ip):
+                if dest_index >= len(dest_ip) - 1:
                     dest_index = 0
 
             service_range_start += 100
-            service_range_end += 100
+
+            if service_range_start > 218001:
+                    service_range_start = 0
+                    service_range_end = 0
+            if service_range_start == 218001:
+                service_range_end += 89
+            else:
+                service_range_end += 100
+            intipadd += 1
 
             # print "Total Rows Added for user %s : %d" % (users[user].strip(), user_index)
 
@@ -118,25 +131,30 @@ def write_cws_file(filename, no_users, logs_per_user, invalid_rows, users, servi
 
             # To make a data row invalid, serviceip contains hardcoded * signs and 0.0.0.0 as destination IP.
             # Username is set by default to INVALID_User. These logs should not be detected during data processing.
+
             new_file.write(
                 "%s\t10.0.1.1\t\tInvalid_User\tGET\t%s\t********************\t80\t\t\t%s\t-\t%d\t%d\t500\timage/jpeg\t0.0.0.0\tc:infr\tdefault\tallow\t\t\t190.180.100.73\t1278716032\t\n" % (
-                    get_new_date(timestamp_format), services[random.randint(0, len(services))].split(":")[0],
-                    user_agent[random.randint(0, len(user_agent))].strip(), s_bytes, r_bytes))
-
+                    get_new_date(timestamp_format), services[random.randint(0, len(services)-1)].split(":")[0],
+                    user_agent[random.randint(0, len(user_agent)-1)].strip(), s_bytes, r_bytes))
 
         print "Total Invalid Rows Added %d" % invalid_rows
 
         # Sorting the file based on time and creating a gzip.
         os.system("sort ../cws_data/%s -k 1 > ../cws_data/%s_sorted.tsv" % (filename, filename))
+
+        print "Starting to Zip the file: " + strftime('%Y-%m-%d %H:%M:%S', gmtime())
         os.system("gzip ../cws_data/%s_sorted.tsv" % filename)
+        print "File completely Zipped: " + strftime('%Y-%m-%d %H:%M:%S', gmtime())
 
         # Removing the unsorted file.
-        os.system("rm ../cws_data/%s" % filename)
-
+        #os.system("rm ../cws_data/%s" % filename)
+        total_rows = int(row_index + invalid_rows)
+        print "Total Rows Added to file: %d" % total_rows
         print "Data File of name ../cws_data/%s_sorted.tsv.gz with %d user(s)." % (filename, no_users)
         print "**************** **********************"
+
     except Exception, e:
-        # logger.exception(e)
+        logger.exception(e)
         print str(e)
     finally:
         new_file.close()
@@ -186,6 +204,29 @@ def get_new_date(current_epoch):
         year) + '-' + month_impl + '-' + day_impl + ' ' + hour_impl + ':' + min_impl + ':' + secs_impl
     return timestamp_format
 
+def prepare_stream_logger(logger, extra, debug_level):
+    '''
+    Prepare a stream logger
+
+    :param logger: logger to use
+    :type logger: logging.Logger instance
+
+    :param extra: Extra information to append in the log. Is a string
+    :type extra: str
+
+    :param filename: Filename to be used to logging. Absolute path needs to be given otherwise in cwd
+    :type filename: str
+
+    :param debug_level: Debug level to set on the logger
+    :type debug_level: one of logging.DEBUG, logging.ERROR, logging.INFO, logging.WARNING
+    '''
+    logger.setLevel(debug_level)
+    #logger.addFilter(UserLogFilter(extra))
+
+    sh = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s-%(message)s')
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
 
 if __name__ == '__main__':
     if len(sys.argv) == 5:
